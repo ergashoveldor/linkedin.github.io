bdea2daf7c1c938f0f1e417e822bdff76c8efee4
eldor-ergashov <eldor7147@gmail.com>
2022-05-01 19:19:18 +0500

    added Sign in button and styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,10 @@ const Login = (props) => {
         <a href='/'>
           <img src="/images/login-logo.svg" alt='' />
         </a>
+        <div>
+          <Join>Join now</Join>
+          <Signin>Sign in</Signin>
+        </div>
       </Nav>
     </Container>
   )
@@ -26,5 +30,53 @@ const Nav = styled.nav`
   position: relative;
   justify-content: space-between;
   flex-wrap: nowrap;
+
+  & > a {
+    width: 135px;
+    height: 34px;
+    @media (max-width: 768px) {
+      padding: 0 5px;
+    }
+  }
 `;
+
+const Join = styled.a`
+  font-size: 16px;
+  padding: 10px 12px;
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  font-weight: bold;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0,0,0,0.9);
+    text-decoration: none;
+  }
+`;
+
+const Signin = styled.a`
+  box-shadow: inset 0 0 0 1px #0a66c2;
+  color: #0a66c2;
+  border-radius: 24px;
+  transition-duration: 167ms;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 40px;
+  padding: 10px 24px;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0);
+
+  &:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+    text-decoration: none;
+  }
+  border: 0;
+  cursor: pointer;
+  text-decoration: none;
+  height: 40px;
+`;
+
+
 export default Login;
