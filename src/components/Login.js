d1dd2d654adc3da641d77c41aaf651c889d8aabf
eldor-ergashov <eldor7147@gmail.com>
2022-05-02 19:17:16 +0500

    added: Sign in with google

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,7 +15,14 @@ const Login = (props) => {
       <Section>
         <Hero>
           <h1>Welcome to your professional community</h1>
+          <img src="/images/login-hero.svg" alt="" />
         </Hero>
+        <Form>
+          <Google>
+            <img src="/images/google.svg" alt="" />
+            Sign in with Google
+          </Google>
+        </Form>
       </Section>
     </Container>
   )
@@ -118,6 +125,50 @@ const Hero = styled.div`
       font-size: 20px;
       line-height: 2;
     }
+  }
+  img {
+    /* z-index: -1; */
+    width: 700px;
+    height: 670px;   
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+    @media (max-width: 768px) {
+        top: 230px;
+        width: initial;
+        height: initial;
+        position: initial;
+    }
+  }
+`;
+
+const Form = styled.div`
+  margin-top: 100px;
+  width: 408px;
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+
+const Google = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  height: 56px;
+  width: 100%;
+  border-radius: 28px;
+  box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%), 
+  inset 0 0 0 2px rgb(0 0 0 / 0%) 
+  inset 0 0 0 1px rgb(0 0 0 / 0);
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.6);
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.75);
   }
 `;
 
