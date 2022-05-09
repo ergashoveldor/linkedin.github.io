37f3d902c0576586e7bd27b1276b256893664f7b
eldor-ergashov <eldor7147@gmail.com>
2022-05-09 15:57:14 +0500

    added: CommunityCard styles

--- src/components/Leftside.js
+++ src/components/Leftside.js
@@ -26,9 +26,32 @@ const Leftside = (props) => {
         <Item>
           <span>
             <img src="/images/item-icon.svg" alt="" />
+            My items
           </span>
         </Item>
       </ArtCard>
+
+      <CommunityCard>
+        <a>
+          <span>Groups</span>
+        </a>
+        <a>
+          <span>
+            Events
+            <img src="/images/plus-icon.svg" alt="" />
+          </span>
+        </a>
+        <a>
+          <span>
+            Follow Hashtags
+          </span>
+        </a>
+        <a>
+          <span>
+            Discover more
+          </span>
+        </a>
+      </CommunityCard>
     </Container>
   )
 }
@@ -96,8 +119,91 @@ const AddPhotoText = styled.div`
   font-weight: 400;
 `;
 
-const Widget = styled.div``;
+const Widget = styled.div`
+  border-bottom: 1px solid rgba(0,0,0,0.15);
+  padding-top: 12px;
+  padding-bottom: 12px;
 
-const Item = styled.div``;
+  & > a {
+    text-decoration: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4px 12px;
+  }
+  &:hover {
+    background-color: rgba(0,0,0,0.08);
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    span {
+      font-size: 12px;
+      line-height: 1.333;
+      &:first-child {
+        color: rgba(0,0,0,0.6);
+      }
+      &:nth-child(2) {
+        color: rgba(0,0,0,1);
+      }
+    }
+  }
+
+  svg {
+    color: rgba(0,0,0,1);
+  }
+`;
+
+const Item = styled.div`
+  border-color: rgba(0,0,0,0.8);
+  text-align: left;
+  padding: 12px;
+  font-size: 12px;
+  display: block;
+  span {
+    display: flex;
+    align-items: center;
+    color: rgba(0,0,0,1);
+    svg {
+      color: rgba(0,0,0,0.6);
+    }
+  }
+
+  &:hover {
+    background-color: rgba(0,0,0,0.08);
+  }
+`;
+
+const CommunityCard = styled(ArtCard)`
+  padding: 8px 0 0;
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  a {
+    color: black;
+    padding: 4px 12px 4px 12px;
+    font-size: 12px;
+    &:hover {
+      color: #0a66c2;
+    }
+    span {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    &:last-child {
+      color: rgba(0,0,0,0.6);
+      text-decoration: none;
+      border-top: 1px solid #d6cec2;
+      padding: 12px;
+      &:hover {
+        background-color: rgba(0,0,0,0.08);
+      }
+    }
+  }
+`;
 
 export default Leftside;
