dd6f1ea04e886903926d3f2f56f5a67c38bbacb4
ergashoveldor <eldor7147@gmail.com>
2022-05-12 12:34:29 -0400

    added: rightside elements and main icon group

--- src/components/Leftside.js
+++ src/components/Leftside.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Leftside = (props) => {
   return (
     <Container>
-      <ArtCard>
+      <ArtCard> 
         <UserInfo>
           <CardBackground />
           <a>
