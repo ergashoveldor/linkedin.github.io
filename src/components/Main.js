ad53614b306afa4f6e355778f1c9b403cd9cf8fe
ergashoveldor <eldor7147@gmail.com>
2022-05-25 12:44:13 +0500

    final project

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import PostModal from './PostModal';
 import { getArticlesAPI } from '../actions';
 import { connect } from 'react-redux';
+import ReactPlayer from 'react-player';
 
 const Main = (props) => {
   const [showModal, setShowModal] = useState('close');
@@ -64,65 +65,73 @@ const Main = (props) => {
               </button>
             </div>
           </ShareBox>
-          <div>
-            <Article>
-              <SharedActor>
-                <a>
-                  <img src="/images/user.svg" alt="" />
-                  <div>
-                    <span>Title</span>
-                    <span>Info</span>
-                    <span>Date</span>
-                  </div>
-                </a>
-                <button>
-                  <img src="/images/ellipsis.svg" alt="" />
-                </button>
-              </SharedActor>
-              <Description>Description</Description>
-              <SharedImg>
-                <a>
-                  <img src="/images/shared-image.jpg" alt="" />
-                </a>
-              </SharedImg>
-              <SocialCounts>
-                <li>
-                  <button>
-                    <img
-                      src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb"
-                      alt=""
-                    />
-                    <img
-                      src="https://static-exp1.licdn.com/sc/h/b1dl5jk88euc7e9ri50xy5qo8"
-                      alt=""
-                    />
-                    <span>75</span>
-                  </button>
-                </li>
-                <li>
-                  <a>2 comments</a>
-                </li>
-              </SocialCounts>
-              <SocialActions>
-                <button>
-                  <img src="/images/like-icon.svg" alt="" />
-                  <span>Like</span>
-                </button>
-                <button>
-                  <img src="/images/comments-icon.svg" alt="" />
-                  <span>Comments</span>
-                </button>
-                <button>
-                  <img src="/images/share-icon.svg" alt="" />
-                  <span>Share</span>
-                </button>
-                <button>
-                  <img src="/images/send-icon.svg" alt="" />
-                  <span>Send</span>
-                </button>
-              </SocialActions>
-            </Article>
-          </div>
+
+          {props.articles.length > 0 &&
+            props.articles.map((article, key) => (
+              <div>
+                <Article key={key}>
+                  <SharedActor>
+                    <a>
+                      <img src={article.actor.image} alt="" />
+                      <div>
+                        <span>{article.actor.title}</span>
+                        <span>{article.actor.description}</span>
+                        <span>{article.actor.date.toDate().toLocaleDateString()}</span>
+                      </div>
+                    </a>
+                    <button>
+                      <img src="/images/ellipsis.svg" alt="" />
+                    </button>
+                  </SharedActor>
+                  <Description>{article.description}</Description>
+                  <SharedImg>
+                    <a>
+                     {
+                       !article.sharedImg && article.video ? <ReactPlayer width={'100%'} url={article.video} /> : (
+                         article.sharedImg && <img src={article.sharedImg}/>  
+                       )
+                     }
+                    </a>
+                  </SharedImg>
+                  <SocialCounts>
+                    <li>
+                      <button>
+                        <img
+                          src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb"
+                          alt=""
+                        />
+                        <img
+                          src="https://static-exp1.licdn.com/sc/h/b1dl5jk88euc7e9ri50xy5qo8"
+                          alt=""
+                        />
+                        <span>75</span>
+                      </button>
+                    </li>
+                    <li>
+                      <a>{article.comments}</a>
+                    </li>
+                  </SocialCounts>
+                  <SocialActions>
+                    <button>
+                      <img src="/images/like-icon.svg" alt="" />
+                      <span>Like</span>
+                    </button>
+                    <button>
+                      <img src="/images/comments-icon.svg" alt="" />
+                      <span>Comments</span>
+                    </button>
+                    <button>
+                      <img src="/images/share-icon.svg" alt="" />
+                      <span>Share</span>
+                    </button>
+                    <button>
+                      <img src="/images/send-icon.svg" alt="" />
+                      <span>Send</span>
+                    </button>
+                  </SocialActions>
+                </Article>
+              </div>
+            ))}
           <PostModal showModal={showModal} handleClick={handleClick} />
         </Container>
       )}
@@ -293,6 +302,8 @@ const SocialCounts = styled.ul`
     margin-right: 5px;
     button {
       display: flex;
+      border: none;
+      background-color: #fff;
     }
   }
 `;
@@ -309,6 +320,8 @@ const SocialActions = styled.div`
     align-items: center;
     padding: 8px;
     color: #006ac2;
+    border: none;
+    background-color: #fff;
 
     @media (min-width: 768px) {
       span {
